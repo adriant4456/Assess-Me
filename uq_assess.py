50455250bb16142b158f8707b0be957c874ff110
Adrian <adriant4456@gmail.com>
2018-08-15 21:04:23 +1000

    Added recursive unwrapper function

--- uq_assess.py
+++ uq_assess.py
@@ -10,11 +10,29 @@ soup = BeautifulSoup(assessment_page, 'lxml')
 
 assessment_table = soup.findAll('table')[6]
 rows = assessment_table.findAll('tr')
+
+def unwrap(content):
+    try:
+        next_contents = content.contents
+        unwrapped_content = unwrap(next_contents)
+        return unwrapped_content
+    except AttributeError:
+        print(content)
+        return content
+
 for i in range(1,len(rows)):
-    for i in rows[i].findAll('div'):
-        if '<i>' in i.contents:
+     for j in rows[i].findAll('div'):
+         content = j.contents
+         if len(content) > 1:
+             for k in content:
+                 print(unwrap(content))
+         else:
+             print(j.text)
+        
+             
             
-    
+
+
 
 
 
